cfb0d815e4866fdee82512c4f9cf32aed407113a
divine-mercyx <divinewilson766@gmail.com>
2025-07-10 11:56:15 +0100

    update: fixed login bugs of json not being a function, done with koojoo app functionality

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.decorate('authenticate', async (request, reply) => {
 
 const start = async () => {
     try {
-        await app.listen({ port: PORT });
+        await app.listen({ port: PORT, host: '0.0.0.0' });
         console.log(`Server is running on port ${PORT}`);
     } catch (err) {
         console.error(err);
